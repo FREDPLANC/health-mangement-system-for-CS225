8afa9c3ba718468dd4081350a62b88506ee0b8a3
tygtygtyg123 <102898334+tygtygtyg123@users.noreply.github.com>
2022-05-08 14:18:37 +0800

    Add files via upload

---
+++ database.h
@@ -0,0 +1,62 @@
+
+template<class T> class relation
+{
+    public:
+        void insert();
+
+    private:
+        list<Block<T>> blocks;
+};
+
+template<class T> class block
+{
+    public:
+        std::vector<T> array;
+        std::vector<T> overflowBuffer;
+        int index;
+
+
+    private:
+        
+
+
+};
+
+class ID
+{
+    public:
+        int id;
+        void getid();
+        void setid();
+
+
+}
+;
+class Person: public ID
+{
+    public:
+
+    private:
+
+};
+class Medical_Status : public ID
+{
+    public:
+
+    private:
+
+    
+};
+class Registration :public ID
+{
+    public:
+
+    private:
+    
+};
+class Treatment :public ID
+{
+    public:
+
+    private:
+};
