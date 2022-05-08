097b9178558e0949ef8948c0d8bf91cc9d8b7387
tygtygtyg123 <102898334+tygtygtyg123@users.noreply.github.com>
2022-05-08 15:49:44 +0800

    Update database.cpp

--- database.cpp
+++ database.cpp
@@ -19,4 +19,50 @@ class ID:: void setid(int t)
    id= t;
 }
 
+template <class T> void Maindata<T>::insert(patient* p)
+{
+    person.insert(p);
+    medical_status.insert(p);
+    registration.insert(p);
+    treatment.insert(p);
+    return;
+}
+template <class T> bool Maindata<T>::search(int id)
+{
+    return person.search(id);
+}
+template <class T> void Maindata<T>::modify(int id,patient* p)
+{
+    person.modify(id,p);
+    medical_status.modify(id,p);
+    registration.modify(id,p);
+    treatment.modify(id,p);
+    return;
+}
+template <class T> void Maindata<T>::modifyperson(int id,patient* p)
+{
+    person.modify(id,p);
+    
+    return;
+}
+template <class T> void Maindata<T>::modifystatus(int id,patient* p)
+{
+    
+    medical_status.modify(id,p);
+    
+    return;
+}
+template <class T> void Maindata<T>::modifyregistration(int id,patient* p)
+{
+    
+    registration.modify(id,p);
+    return;
+}
+template <class T> void Maindata<T>::modifytreatment(int id,patient* p)
+{
+    
+    treatment.modify(id,p);
+    return;
+}
+
 
