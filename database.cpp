fcaa79fa15ebe6b6a6b2352d19c7fe4e06aeb6f1
tygtygtyg123 <102898334+tygtygtyg123@users.noreply.github.com>
2022-05-08 14:29:47 +0800

    Add files via upload

---
+++ database.cpp
@@ -0,0 +1,22 @@
+#include <list>
+#include <algorithm>
+#include <cstdlib>
+#include <string>
+using namespace std;
+#include "database.h"
+
+template <class T> void relation<T>::insert()
+{
+    //加油哦；
+}
+class ID:: int getid()
+{
+    return id;
+}
+
+class ID:: void setid()
+{
+   //
+}
+
+
